9a07c48131f44f77004241c5a61d1538e22f7f4a
davidEvinced <david.ichilov@evinced.com>
2024-10-01 08:43:06 +0300

    initial copy pasta

---
+++ src/app/angular-docs/components/table-of-contents/table-of-contents.component.ts
@@ -0,0 +1,43 @@
+/*!
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.dev/license
+ */
+
+import {ChangeDetectionStrategy, Component, Input, computed, inject} from '@angular/core';
+import {RouterLink} from '@angular/router';
+import {TableOfContentsLevel} from '../../interfaces';
+import {TableOfContentsLoader, TableOfContentsScrollSpy} from '../../services';
+import {IconComponent} from '../icon/icon.component';
+
+@Component({
+  selector: 'docs-table-of-contents',
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './table-of-contents.component.html',
+  styleUrls: ['./table-of-contents.component.scss'],
+  imports: [RouterLink, IconComponent],
+})
+export class TableOfContents {
+  // Element that contains the content from which the Table of Contents is built
+  @Input({required: true}) contentSourceElement!: HTMLElement;
+
+  private readonly scrollSpy = inject(TableOfContentsScrollSpy);
+  private readonly tableOfContentsLoader = inject(TableOfContentsLoader);
+  tableOfContentItems = this.tableOfContentsLoader.tableOfContentItems;
+
+  activeItemId = this.scrollSpy.activeItemId;
+  shouldDisplayScrollToTop = computed(() => !this.scrollSpy.scrollbarThumbOnTop());
+  TableOfContentsLevel = TableOfContentsLevel;
+
+  ngAfterViewInit() {
+    this.tableOfContentsLoader.buildTableOfContent(this.contentSourceElement);
+    this.scrollSpy.startListeningToScroll(this.contentSourceElement);
+  }
+
+  scrollToTop(): void {
+    this.scrollSpy.scrollToTop();
+  }
+}
